7d3db8a2b26d611c49dc65bd863de07dfdf5057b
Sam Harvey <sam.harvey@ea.govt.com>
2019-11-07 15:48:21 +1300

    Update tests to use setUpClass to set up the temp dir on databricks once.

--- tests/test_databricks_api_utils.py
+++ tests/test_databricks_api_utils.py
@@ -3,35 +3,39 @@
 
 """Tests for `databricks_api_utils` package."""
 
-
 import unittest
 import os
 from databricks_api_utils import databricks_api_utils
 
 
+testing_dir = '/tmp-db-api-utils/'
+
+
 class TestDatabricksApiUtils(unittest.TestCase):
     """Tests for `databricks_api_utils` package."""
 
+    @classmethod
+    def setUpClass(cls):
+        cls.db_instance = os.environ['DATABRICKS_INSTANCE']
+        cls.db_token = os.environ['DATABRICKS_TOKEN']
+        cls.db_api = databricks_api_utils.DatabricksAPI(host=cls.db_instance, token=cls.db_token)
+        cls.db_api.workspace.mkdirs(testing_dir)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down test fixtures, if any."""
+        if cls.db_api.client.verify:
+            cls.db_api.workspace.delete(testing_dir, recursive=True)
+
     def setUp(self):
         """Set up test fixtures, if any."""
-        self.db_instance = os.environ['DATABRICKS_INSTANCE']
-        self.db_token = os.environ['DATABRICKS_TOKEN']
-        self.db_api = databricks_api_utils.DatabricksAPI(host=self.db_instance, token=self.db_token)
+        if not self.db_api.client.verify:
+            self.fail('client not verified')
 
-        if self.db_api.client.verify:
-            self.db_api.workspace.mkdirs('/tmp/')
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-        if self.db_api.client.verify:
-            self.db_api.workspace.delete('/tmp/', recursive=True)
-
-
-
-    def test_databricks_connection(self):
-        """That our connection is working."""
-        self.assertTrue(self.db_api.client.verify)
+    # def tearDown(self):
+    #     """Tear down test fixtures, if any."""
+    #     if self.db_api.client.verify:
+    #         self.db_api.workspace.delete(testing_dir, recursive=True)
 
     def test_databricks_import(self):
         self.assertTrue(True)
-
